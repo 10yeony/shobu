73b37c5401b4a95ce2ec5b372ae3c31a8dc02618
10yeony <10yeony@gmail.com>
2020-06-26 07:58:30 +0900

    Edit DriverManager ServerInfo for Unit Test

--- shobu/src/com/shobu/config/ServerInfo.java
+++ shobu/src/com/shobu/config/ServerInfo.java
@@ -2,8 +2,13 @@ package com.shobu.config;
 
 public interface ServerInfo {
 	String DRIVER_NAME = "com.mysql.cj.jdbc.Driver"; 
-	String URL = "jdbc:mysql://127.0.0.1:3306/shobu?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
-	//String URL = "jdbc:mysql://192.168.0.84:3306/shobu?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
+	
+	//집에서 단위 테스트 할 때 로컬 DB 서버 주소
+	//String URL = "jdbc:mysql://127.0.0.1:3306/shobu?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
+	
+	//학원에서 단위 테스트할 때 민호씨 DB 서버 주소
+	String URL = "jdbc:mysql://192.168.0.84:3306/shobu?serverTimezone=UTC&useUnicode=yes&characterEncoding=UTF-8";
+	
 	String USER = "root";
 	String PASS = "1234";
 }
