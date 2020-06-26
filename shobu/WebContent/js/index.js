f3e7f662734b0484aa9e9412db501bab7be22b51
Jaennie <je028770@gmail.com>
2020-06-26 15:50:02 +0900

    index rank ajax로 수정

--- shobu/WebContent/js/index.js
+++ shobu/WebContent/js/index.js
@@ -6,8 +6,23 @@ $(function() {
 		
 		success:function(r){
 			console.log("Main.jsp get Json");
-			console.log("data.teams:"+r.teams);
-			$("#teamrank").html(r.teams);
+			
+			$.each(r.teams,function(index,teams){
+				$("#teamrank").append("<tr>");
+				$("#teamrank").append("<td>"+teams.ranking+"</td>");
+				$("#teamrank").append("<td>"+teams.teamName+"</td>");
+				$("#teamrank").append("<td>"+teams.win+"</td>");
+				$("#teamrank").append("<td>"+teams.draw+"</td>");
+				$("#teamrank").append("<td>"+teams.lose+"</td>");
+				$("#teamrank").append("<td>"+teams.rate+"</td>");
+				$("#teamrank").append("<td>"+teams.distance+"</td>");
+				$("#teamrank").append("<td>"+teams.teamAVG+"</td>");
+				$("#teamrank").append("<td>"+teams.teamEra+"</td>");
+				$("#teamrank").append("<td>"+teams.stream+"</td>");
+				$("#teamrank").append("<td>"+teams.games10+"</td>");
+				$("#teamrank").append("</tr>");
+			});//~each
+			
 		}//~callback
 		
 	});//~ajax
