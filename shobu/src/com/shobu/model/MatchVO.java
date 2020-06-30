142b5bf5411b6bb3b15b302bf3efff902d39abff
Jaennie <je028770@gmail.com>
2020-06-30 15:33:33 +0900

    matchVO수정

--- shobu/src/com/shobu/model/MatchVO.java
+++ shobu/src/com/shobu/model/MatchVO.java
@@ -4,12 +4,10 @@ public class MatchVO {
 	private String date;
 	private String time;
 	private String home;
-	private String homeCode;
 	private String homeImg;
 	private String homeColor;
 	private double homeRatio;
 	private String away;
-	private String awayCode;
 	private String awayImg;
 	private String awayColor;
 	private double awayRatio;
@@ -31,19 +29,17 @@ public class MatchVO {
 		this.place = place;
 	}
 	
-	public MatchVO(String date, String time, String home, String homeCode, String homeImg, String homeColor,
-			double homeRatio, String away, String awayCode, String awayImg, String awayColor, double awayRatio,
+	public MatchVO(String date, String time, String home, String homeImg, String homeColor,
+			double homeRatio, String away, String awayImg, String awayColor, double awayRatio,
 			String homePitcher, String awayPitcher, String place) {
 		super();
 		this.date = date;
 		this.time = time;
 		this.home = home;
-		this.homeCode = homeCode;
 		this.homeImg = homeImg;
 		this.homeColor = homeColor;
 		this.homeRatio = homeRatio;
 		this.away = away;
-		this.awayCode = awayCode;
 		this.awayImg = awayImg;
 		this.awayColor = awayColor;
 		this.awayRatio = awayRatio;
@@ -155,30 +151,12 @@ public class MatchVO {
 	public void setAwayColor(String awayColor) {
 		this.awayColor = awayColor;
 	}
-	
-	
-
-	public String getHomeCode() {
-		return homeCode;
-	}
-
-	public void setHomeCode(String homeCode) {
-		this.homeCode = homeCode;
-	}
-
-	public String getAwayCode() {
-		return awayCode;
-	}
-
-	public void setAwayCode(String awayCode) {
-		this.awayCode = awayCode;
-	}
 
 	@Override
 	public String toString() {
-		return "MatchVO [date=" + date + ", time=" + time + ", home=" + home + ", homeCode=" + homeCode + ", homeImg="
-				+ homeImg + ", homeColor=" + homeColor + ", homeRatio=" + homeRatio + ", away=" + away + ", awayCode="
-				+ awayCode + ", awayImg=" + awayImg + ", awayColor=" + awayColor + ", awayRatio=" + awayRatio
+		return "MatchVO [date=" + date + ", time=" + time + ", home=" + home + ", homeImg="
+				+ homeImg + ", homeColor=" + homeColor + ", homeRatio=" + homeRatio + ", away=" + away + ", awayImg=" + awayImg + 
+				", awayColor=" + awayColor + ", awayRatio=" + awayRatio
 				+ ", homePitcher=" + homePitcher + ", awayPitcher=" + awayPitcher + ", place=" + place + "]";
 	}
 
