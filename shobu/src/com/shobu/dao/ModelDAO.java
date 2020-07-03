348ca0c96d4da10f6f18865d40c7fbcf21d816da
10yeony <10yeony@gmail.com>
2020-07-03 17:44:35 +0900

    change

--- shobu/src/com/shobu/dao/ModelDAO.java
+++ shobu/src/com/shobu/dao/ModelDAO.java
@@ -19,7 +19,6 @@ import com.shobu.model.PlayerVO;
 import com.shobu.model.ResultVO;
 import com.shobu.model.TeamInfoVO;
 import com.shobu.model.TeamVO;
-import com.shobu.model.TotoVO2;
 
 public interface ModelDAO {
 	Connection getConnection() throws SQLException ;
